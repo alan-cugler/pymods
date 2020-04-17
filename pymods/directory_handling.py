1f219b16da97850e27588d8e2058c1a558061aba
Alan J Cugler <alan.cugler101@gmail.com>
2020-04-16 22:48:09 -0500

    was using mkdir wrong from the pathlib package

--- pymods/directory_handling.py
+++ pymods/directory_handling.py
@@ -8,7 +8,9 @@ def extract_mods(mod_root_path, mod_comp_path):
 
     for file in mod_comp_path.iterdir():
         if file.suffix in ('.7z','.rar','.zip'):
-            patoolib.extract_archive(file.name, outdir=mod_root_path)
+            mod_full_path = PurePath(mod_root_path).joinpath(PurePath(file.name))
+            Path.mkdir(PurePath(mod_full_path))
+            patoolib.extract_archive(file.name, outdir=mod_full_path)
 
 
 def create_mod_list(mod_root_path):
@@ -22,23 +24,25 @@ def create_mod_list(mod_root_path):
     return mod_list
 
 
-def rename_mod(mod_root_path, mod_dir):
+#def rename_mod(mod_root_path, mod_dir):
     # parse mod name to readable mod for migration
-
-    if mod_dir in
-
+    #['se','sse','SE','SSE']
 
 
 def simplify_mod_dir(mod_root_path, mod_dir):
     # Create replacement directory.
 
-    new_mod_dir = rename_mod(mod_root_path, mod_dir)
-    Path.mkdir(PurePath(mod_root_path).joinpath(PurePath(new_mod_dir)))
-
     mod_key_dir_list = ['textures','meshes','skse','source']
+
+    new_mod_dir = mod_dir + '_test'
     full_path_mod = PurePath(mod_root_path).joinpath(PurePath(new_mod_dir))
 
+    full_path_mod.mkdir
     for key_dir in mod_key_dir_list:
-        Path.mkdir(PurePath(full_path_mod).joinpath(PurePath(key_dir)))
-
+        sub_mod_dir = PurePath(full_path_mod).joinpath(PurePath(key_dir))
+        sub_mod_dir.mkdir
+        if key_dir == 'skse':
+            skse_path = PurePath(full_path_mod).joinpath(PurePath(key_dir))
+            skse_plug_dir = PurePath(skse_path).joinpath(PurePath('plugins'))
+            skse_plug_dir.mkdir
 
