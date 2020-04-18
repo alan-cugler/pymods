f502e68b0ea96ce2f0ac8d6f99eb78476b01602b
Alan Cugler <alan.cugler101@gmail.com>
2020-04-18 10:58:35 -0500

    cleaned out the .join code for a simple '/'

--- pymods/directory_handling.py
+++ pymods/directory_handling.py
@@ -4,17 +4,14 @@ import patoolib
 
 
 def extract_mods(mod_root_path, mod_comp_path):
-    # Unpacks all compressed mods to the mod directory.
-
     for file in mod_comp_path.iterdir():
         if file.suffix in ('.7z','.rar','.zip'):
-            mod_full_path = PurePath(mod_root_path).joinpath(PurePath(file.name))
-            Path.mkdir(PurePath(mod_full_path))
+            mod_full_path = mod_root_path / file.name
+            mod_full_path.mkdir(exist_ok=True)
             patoolib.extract_archive(file.name, outdir=mod_full_path)
 
 
 def create_mod_list(mod_root_path):
-    # This creates a list of available mods from a directory.
     mod_list = []
 
     for file in mod_root_path.iterdir():
@@ -24,25 +21,19 @@ def create_mod_list(mod_root_path):
     return mod_list
 
 
-#def rename_mod(mod_root_path, mod_dir):
-    # parse mod name to readable mod for migration
-    #['se','sse','SE','SSE']
-
-
 def simplify_mod_dir(mod_root_path, mod_dir):
-    # Create replacement directory.
+    new_mod_dir = mod_dir + '_test'
+    full_path_mod = mod_root_path / new_mod_dir
+    full_path_mod.mkdir(exist_ok=True)
 
     mod_key_dir_list = ['textures','meshes','skse','source']
-
-    new_mod_dir = mod_dir + '_test'
-    full_path_mod = PurePath(mod_root_path).joinpath(PurePath(new_mod_dir))
-
-    full_path_mod.mkdir
     for key_dir in mod_key_dir_list:
-        sub_mod_dir = PurePath(full_path_mod).joinpath(PurePath(key_dir))
-        sub_mod_dir.mkdir
+        sub_mod_dir = full_path_mod / key_dir
+        sub_mod_dir.mkdir(exist_ok=True)
         if key_dir == 'skse':
-            skse_path = PurePath(full_path_mod).joinpath(PurePath(key_dir))
-            skse_plug_dir = PurePath(skse_path).joinpath(PurePath('plugins'))
-            skse_plug_dir.mkdir
+            skse_plug_dir = full_path_mod / key_dir / 'plugins'
+            skse_plug_dir.mkdir(exist_ok=True)
+
+
+def catalog_creation():
 
