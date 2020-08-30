11a289ac55e0535aa77a717f536fd30c538df61a
Alan Cugler <alan.cugler101@gmail.com>
2020-08-30 15:38:20 -0500

    buttons working with spacing within two sibling frames.

--- pymods/main.py
+++ pymods/main.py
@@ -1,8 +1,9 @@
-from tkinter import *
-from tkinter import ttk
+#!/usr/bin/python3
+
+import tkinter as tk
 import platform
 
-root = Tk()
+root = tk.Tk()
 root.title('PyMods Manager')
 
 system = platform.system()
@@ -14,8 +15,24 @@ elif system == '':
     expectation = "Expected: 'Linux' 'Windows' or 'Darwin', Received: "
     raise OSError(expectation + system)
 
+lframe = tk.Frame(root)
+lframe.grid(column=0, row=0, columnspan=7)
+lframe.grid_columnconfigure(14, minsize=100)
 
-mainframe = ttk.Frame(root, padding='3 3 12 12')
-mainframe.grid(column=0, row=0, sticky=(N, E, S, W))
+top_left = ['L1','L2','L3','L4','L5','L6','L7']
+count = 0
+for button in top_left:
+    button = tk.Button(lframe, text=button)
+    button.grid(row=0, column=count)
+    count += 1
+
+rframe = tk.Frame(root)
+rframe.grid(column=15, row=0)
+
+top_right = ['R1','R2','R3']
+for button in top_right:
+    button = tk.Button(rframe, text=button)
+    button.grid(row=0, column=count)
+    count += 1
 
 root.mainloop()
